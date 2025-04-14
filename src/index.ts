35980249b39bab5dc300d3c5bce7aca98343700c
Glauber Portella <glauberportella@gmail.com>
2025-04-14 10:50:11 -0300

    gitignore

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import { CloudTasksClient } from '@google-cloud/tasks';
+import { google } from '@google-cloud/tasks/build/protos/protos';
+import { CloudTaskOptions } from './types';
+
+export class CloudTaskDispatcher {
+    private client: CloudTasksClient;
+
+    constructor() {
+        this.client = new CloudTasksClient();
+    }
+
+    async dispatch(options: CloudTaskOptions): Promise<google.cloud.tasks.v2.ITask> {
+        const {
+            projectId,
+            location,
+            queueId,
+            url,
+            method = 'POST',
+            payload,
+            scheduleTime,
+            serviceAccountEmail,
+        } = options;
+
+        const parent = this.client.queuePath(projectId, location, queueId);
+        
+        const task: google.cloud.tasks.v2.ITask = {
+            httpRequest: {
+                httpMethod: method,
+                url,
+            },
+        };
+
+        if (payload) {
+            task.httpRequest!.body = Buffer.from(JSON.stringify(payload)).toString('base64');
+            task.httpRequest!.headers = {
+                'Content-Type': 'application/json',
+            };
+        }
+
+        if (scheduleTime) {
+            task.scheduleTime = {
+                seconds: scheduleTime.getTime() / 1000,
+            };
+        }
+
+        if (serviceAccountEmail) {
+            task.httpRequest!.oidcToken = {
+                serviceAccountEmail,
+                audience: url,
+            };
+        }
+
+        const [response] = await this.client.createTask({
+            parent,
+            task,
+        });
+
+        return response;
+    }
+}
